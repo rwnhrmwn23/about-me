d0c3802a6db39bd0a21116c07257c7c6feaa5959
haryarrahman <haryarrahman7@gmail.com>
2023-09-18 17:26:33 +0700

    fix: cta area buttong

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -11,12 +11,7 @@ const Footer = ({ colorTheme }: { colorTheme: string }) => {
     <div className="flex px-[16px] lg:px-[60px] py-[40px]">
       <Grid container alignItems="center" spacing="20px">
         <Grid item lg={3} md={12} xs={12}>
-          <div
-            className="flex flex-row items-end gap-[8px] cursor-pointer"
-            onClick={() =>
-              window.open("mailto:haryarrahman14@gmail.com", "_blank")
-            }
-          >
+          <div className="flex flex-row items-end gap-[8px] ">
             <Image
               style={{
                 height: "18px",
@@ -24,19 +19,19 @@ const Footer = ({ colorTheme }: { colorTheme: string }) => {
               alt="gmail"
               src={colorTheme == "light" ? emaillight : emaildark}
             />
-            <p className="typography-h200 dark:hover:text-base-light dark:text-base-light-active text-base-dark-active hover:text-base-light-active">
+            <p
+              className="typography-h200 dark:hover:text-base-light dark:text-base-light-active text-base-dark-active hover:text-base-light-active cursor-pointer"
+              onClick={() =>
+                window.open("mailto:haryarrahman14@gmail.com", "_blank")
+              }
+            >
               haryarrahman14@gmail.com
             </p>
           </div>
         </Grid>
 
         <Grid item lg={3} md={12} xs={12}>
-          <div
-            className="flex flex-row items-end gap-[8px] cursor-pointer"
-            onClick={() =>
-              window.open("https://www.linkedin.com/in/hary-arrahman", "_blank")
-            }
-          >
+          <div className="flex flex-row items-end gap-[8px] ">
             <Image
               style={{
                 height: "18px",
@@ -44,7 +39,15 @@ const Footer = ({ colorTheme }: { colorTheme: string }) => {
               alt="linkedin"
               src={colorTheme == "light" ? linkedinlight : linkedindark}
             />
-            <p className="typography-h200 dark:hover:text-base-light dark:text-base-light-active text-base-dark-active hover:text-base-light-active">
+            <p
+              className="typography-h200 dark:hover:text-base-light dark:text-base-light-active text-base-dark-active hover:text-base-light-active cursor-pointer"
+              onClick={() =>
+                window.open(
+                  "https://www.linkedin.com/in/hary-arrahman",
+                  "_blank"
+                )
+              }
+            >
               hary-arrahman
             </p>
           </div>
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -31,12 +31,7 @@ const Hero = ({ colorTheme }: { colorTheme: string }) => {
 
           <Grid container spacing="20px">
             <Grid item lg={3} md={12} xs={12}>
-              <div
-                className="flex flex-row items-end gap-[8px] cursor-pointer"
-                onClick={() =>
-                  window.open("mailto:haryarrahman14@gmail.com", "_blank")
-                }
-              >
+              <div className="flex flex-row items-end gap-[8px]">
                 <Image
                   style={{
                     height: "18px",
@@ -44,22 +39,19 @@ const Hero = ({ colorTheme }: { colorTheme: string }) => {
                   alt="gmail"
                   src={colorTheme == "light" ? emaillight : emaildark}
                 />
-                <p className="typography-h200 dark:hover:text-base-light dark:text-base-light-active text-base-dark-active hover:text-base-light-active">
+                <p
+                  className="typography-h200 dark:hover:text-base-light dark:text-base-light-active text-base-dark-active hover:text-base-light-active  cursor-pointer"
+                  onClick={() =>
+                    window.open("mailto:haryarrahman14@gmail.com", "_blank")
+                  }
+                >
                   haryarrahman14@gmail.com
                 </p>
               </div>
             </Grid>
 
             <Grid item lg={3} md={12} xs={12}>
-              <div
-                className="flex flex-row items-end gap-[8px] cursor-pointer"
-                onClick={() =>
-                  window.open(
-                    "https://www.linkedin.com/in/hary-arrahman",
-                    "_blank"
-                  )
-                }
-              >
+              <div className="flex flex-row items-end gap-[8px]">
                 <Image
                   style={{
                     height: "18px",
@@ -67,7 +59,15 @@ const Hero = ({ colorTheme }: { colorTheme: string }) => {
                   alt="linkedin"
                   src={colorTheme == "light" ? linkedinlight : linkedindark}
                 />
-                <p className="typography-h200 dark:hover:text-base-light dark:text-base-light-active text-base-dark-active hover:text-base-light-active">
+                <p
+                  className="typography-h200 dark:hover:text-base-light dark:text-base-light-active text-base-dark-active hover:text-base-light-active cursor-pointer"
+                  onClick={() =>
+                    window.open(
+                      "https://www.linkedin.com/in/hary-arrahman",
+                      "_blank"
+                    )
+                  }
+                >
                   hary-arrahman
                 </p>
               </div>
